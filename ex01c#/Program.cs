d6ac2505e5945bf3990983222bfbd2365109ac1b
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2021-08-30 23:44:12 -0300

    Cria projeto
    Exercitando comandos básicos

---
+++ ex01c#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex01c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int var1 = 10;
+            int var2 = 20;
+            int var3 = 30;
+
+            Console.WriteLine(var1);
+            Console.WriteLine(var2);
+            Console.WriteLine(var3);
+
+            Console.WriteLine(var1 < var2);
+            Console.WriteLine(var1 == var3);
+
+            var1++;
+            var3--;
+
+            Console.WriteLine(var1);
+            Console.WriteLine(var3);
+
+        }
+    }
+}
